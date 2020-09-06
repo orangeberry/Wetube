4786066a8cbc05abbe4f78455a3888d8833aeb10
orangeberry <orangeberry@kakao.com>
2020-09-06 18:57:01 +0900

    .

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import {userRouter} from "./router";
+const app = express();
+
+const handleHome = (req, res) => res.send('Hi This is your homeee');
+
+
+const handleProfile = (req,res) => res.send("This is your profile");
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
