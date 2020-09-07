7b8dacc58ea0f916e498fd23e1ec36768bd9b4bd
orangeberry <orangeberry@kakao.com>
2020-09-07 23:44:40 +0900

    .

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
+
 const app = express();
 
-const handleHome = (req, res) => res.send('Hi This is your homeee');
-
-
-const handleProfile = (req,res) => res.send("This is your profile");
-
 app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 
-app.get("/", handleHome);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
