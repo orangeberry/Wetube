081e73bfde3c0fbe3986aa32e6b04a7ea2476919
orangeberry <orangeberry@kakao.com>
2020-09-09 00:05:43 +0900

    .

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WeTube
 
 Cloning Youtube with Vallina and NodeJS!!
+
+## Pages:
+
+- [] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [] User Detail
+- [] Edit Profile
+- [] Change password
+- [] Upload
+- [] Video Detail
+- [] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,12 +2,12 @@ import express from "express";
 import routes from "../routes";
 import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
 
