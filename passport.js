a55a3a332b1d9c5dbefd501cdd04a42a5f40e3ea
orangeberry <orangeberry@kakao.com>
2020-09-12 17:44:51 +0900

    .

---
+++ passport.js
@@ -0,0 +1,19 @@
+import passport from "passport";
+import GithubStrategy from "passport-github";
+import { githubLoginCallback } from "./controllers/userController";
+import User from "./models/User";
+import routes from "./routes";
+
+passport.use(User.createStrategy());
+
+passport.use(new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: `http://localhost:4000${routes.githubCallback}`
+}, githubLoginCallback
+),
+
+);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
