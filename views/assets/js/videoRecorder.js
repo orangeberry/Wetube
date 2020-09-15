1182f0079ce535a6503f922bbdcc7debd64400b0
orangeberry <orangeberry@kakao.com>
2020-09-15 16:57:51 +0900

    .

--- views/assets/js/videoRecorder.js
+++ views/assets/js/videoRecorder.js
@@ -2,7 +2,35 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = (event) => {
+    const { data: videoFile } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
+}
+
+
+const stopRecording = () => {
+    videoRecorder.stop();
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("clilck", getVideo);
+    recordBtn.innerHTML = "Start Recording";
+}
+
+const startRecording = () => {
+    videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+    recordBtn.addEventListener("click", stopRecording);
+}
+
+
+const getVideo = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
@@ -11,14 +39,18 @@ const startRecording = async () => {
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
+        recordBtn.innerHTML = "Stop Recording";
+        streamObject = stream;
+        startRecording();
     } catch (error) {
         recordBtn.innerHTML = "😞 Cannot Record";
-        recordBtn.removeEventListener("click", startRecording);
+    } finally {
+        recordBtn.removeEventListener("click", getVideo);
     }
 }
 
 function init() {
-    recordBtn.addEventListener("clilck", startRecording);
+    recordBtn.addEventListener("clilck", getVideo);
 }
 
 if (recorderContainer) {
